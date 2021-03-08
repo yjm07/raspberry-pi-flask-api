fb40fa90bbf55310d936181f93dd1957e5631612
yjm07 <yjm9507@yonsei.ac.kr>
2021-03-08 14:27:02 +0900

    feat: Show signal, freq with current wifi info

--- berry/script/connected_wifi.py
+++ berry/script/connected_wifi.py
@@ -3,7 +3,7 @@ import subprocess
 
 def connected_wifi():
 
-    proc = subprocess.Popen("iw wlan0 link | grep SSID | cut -d ' ' -f 2", 
+    proc = subprocess.Popen("iw wlan0 link | egrep 'SSID|freq|signal' | cut -d ' ' -f 2",
                             shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
     output = stdout.decode()
@@ -15,4 +15,4 @@ def connected_wifi():
     if len(output) == 0:
         return "Not connected"
         
-    return output.rstrip("\n")
+    return output.split()
