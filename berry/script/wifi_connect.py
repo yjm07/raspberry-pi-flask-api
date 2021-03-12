5ec12fc9878ccf03b51b9cdcf9792316a0e1c8e2
yjm07 <yjm9507@yonsei.ac.kr>
2021-03-12 19:04:27 +0900

    feat: Add wifi-connnect & .sh, .conf files used
    Minor fix in wifi_scan.py
    Add methods in wifi_connect.py to connect wifi with ssid and check if psk is right
    Make wpa.sh, temp.comf, temp_origin.conf files used in wifi_connect.py

--- berry/script/wifi_connect.py
+++ berry/script/wifi_connect.py
@@ -1,14 +1,101 @@
 import subprocess
-
-#setting architecture
-def connect(ssid, psk):
+from time import sleep
 
 
-def is_known_wifi():
+ssid_list = set()
+wpa_supplicant = '/etc/wpa_supplicant/wpa_supplicant.conf'
+user_directory = './temp.conf'
+origin_user_directory = './temp_origin.conf'
+
+
+def is_known_wifi(ssid):
+    return ssid in ssid_list
+
+
+def connect(ssid, psk, auto_reconnect):
+    if is_known_wifi(ssid):
+        id = id_wpa_cli(ssid)
+        connect_wpa_cli(id)
+    else:
+        write_temp_wpa_supplicant(ssid, psk)
+        reconnect_wpa_supplicant(user_directory)
+        psk_right = is_psk_right()
+        if psk_right == 'connected':
+            ssid_list.add(ssid)
+            write_wpa_supplicant(ssid, psk)
+            print("connected!")
+        else:
+            print("wrong psw")
+        flush(user_directory)
+    print("done")
+    return
+
+
+def flush(dir):
+    subprocess.call(['sudo', 'cp', origin_user_directory, dir])
 
 
 def is_psk_right():
+    subprocess.call('wpa_cli reconfigure > /dev/null', shell=True)
+    proc = subprocess.Popen(['./wpa.sh'],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+
+    if stderr != b'':
+        print(stderr.decode())
+    else:
+        print('output')
+        return stdout.decode().rstrip()
 
 
-def write_wpa_supplicant():
-    
+def connect_wpa_cli(id):
+    subprocess.call(['wpa_cli', '-i', 'wlan0', 'select_network', id])
+
+
+def remove_wpa_cli(id):
+    subprocess.call(['wpa_cli', '-i', 'wlan0', 'remove_network', id])
+
+
+def id_wpa_cli(ssid):
+    proc = subprocess.Popen(f'wpa_cli -i wlan0 list_networks | grep {ssid}',
+                            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    output = stdout.decode()
+
+    if stderr != b'':
+        print(stderr.decode())
+    if len(stdout) == 0:
+        return -1
+    return output.split()[0]
+
+
+def reconnect_wpa_supplicant(dir):
+    MyOut1 = subprocess.Popen(['sudo', 'killall', 'wpa_supplicant'])
+    MyOut1.wait()
+    sleep(1)
+    print('kill all wpa_supplicant end')
+
+    MyOut2 = subprocess.Popen(['sudo', 'wpa_supplicant','-B', '-iwlan0', '-c', dir, '-f/var/log/wpa_supplicant.log'])
+    MyOut2.wait()
+    sleep(1)
+    print('wpa_supplicant setup end')
+
+    print('reconnect end')
+
+
+def write_wpa_supplicant(ssid, psk):
+    proc = subprocess.Popen(f"wpa_passphrase '{ssid}' {psk} | sudo tee -a {wpa_supplicant}", 
+                            shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout,stderr = proc.communicate()
+    print('write wpa end')
+
+
+def write_temp_wpa_supplicant(ssid, psk):
+    proc = subprocess.Popen(f"wpa_passphrase '{ssid}' {psk} | sudo tee -a {user_directory}", 
+                            shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout,stderr = proc.communicate()
+    print('write temp wpa end')
+
+
+
+
---
+++ berry/script/wpa.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function poke {
+    while true
+    do
+        printf '\n'
+        sleep 1
+    done
+}
+
+function watch {
+    (poke) | wpa_cli | while read line
+    do
+        case "$line" in
+            *'CTRL-EVENT-ASSOC-REJECT'* | *'auth_failures'*)
+                echo "incorrect key"
+                return
+            ;;
+            *'CTRL-EVENT-CONNECTED'*)
+                echo "connected"
+                return
+        esac
+    done
+}
+
+wpa_cli disable_network 0 > /dev/null
+wpa_cli enable_network 0 > /dev/null
+
+watch
