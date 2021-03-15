31c1777727640ae9526fa1ebae35b4564ef5f356
yjm07 <yjm9507@yonsei.ac.kr>
2021-03-15 11:35:54 +0900

    feat: Fix in wifi_connect.py and Add comments
    New feature for adding priority to the newly connected ssid
    Add comments to explain the flow

--- berry/script/wifi_connect.py
+++ berry/script/wifi_connect.py
@@ -2,41 +2,63 @@ import subprocess
 from time import sleep
 
 
-ssid_list = set()
+ssid_list = set()   # ssid list that have been connected once
 wpa_supplicant = '/etc/wpa_supplicant/wpa_supplicant.conf'
 user_directory = './temp.conf'
 origin_user_directory = './temp_origin.conf'
 
 
-def is_known_wifi(ssid):
-    return ssid in ssid_list
-
-
-def connect(ssid, psk, auto_reconnect):
+def connect(ssid, psw, auto_reconnect):
+    """
+    Main method for connecting wifi.
+    Input: {'ssid': string, 'password': string, 'auto_reconnect': boolean}
+    """
+    # If already known, use wpa_cli
     if is_known_wifi(ssid):
         id = id_wpa_cli(ssid)
         connect_wpa_cli(id)
+    # Write to temp_wpa_supplicant.conf and check if psw is right
     else:
-        write_temp_wpa_supplicant(ssid, psk)
+        write_wpa_supplicant(ssid, psw, user_directory)
         reconnect_wpa_supplicant(user_directory)
-        psk_right = is_psk_right()
-        if psk_right == 'connected':
+
+        psw_right = is_psk_right()
+        # If psw is right, add ssid to list and write to wpa_supplicant.conf
+        if psw_right == 'connected':
             ssid_list.add(ssid)
-            write_wpa_supplicant(ssid, psk)
-            print("connected!")
+            write_wpa_supplicant(ssid, psw, wpa_supplicant)
+            reconnect_wpa_supplicant(wpa_supplicant)
+            # Set priority of newly added ssid
+            id = id_wpa_cli(ssid)
+            set_priority(id, id)
+            print("connected")
+        # If psw is wrong, just reconnect to wpa_supplicant.conf
         else:
             print("wrong psw")
+            reconnect_wpa_supplicant(wpa_supplicant)
         flush(user_directory)
+
     print("done")
     return
 
 
+    # if auto_reconnect:
+        # ssid_list.remove(ssid)
+        # remove_wpa_cli(id)
+    # else:
+
+# Check ssid list if already known
+def is_known_wifi(ssid):
+    return ssid in ssid_list
+
+# Flush temp_wpa_supplicant.conf back to the state before
 def flush(dir):
     subprocess.call(['sudo', 'cp', origin_user_directory, dir])
 
-
+# Reconfigure wpa_cli to update wpa_supplicant.conf and check psk
 def is_psk_right():
     subprocess.call('wpa_cli reconfigure > /dev/null', shell=True)
+    sleep(1)
     proc = subprocess.Popen(['./wpa.sh'],
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
@@ -47,15 +69,15 @@ def is_psk_right():
         print('output')
         return stdout.decode().rstrip()
 
-
+# Connect to network of the id with wpa_cli
 def connect_wpa_cli(id):
     subprocess.call(['wpa_cli', '-i', 'wlan0', 'select_network', id])
 
-
+# Remove id from wpa_cli list
 def remove_wpa_cli(id):
     subprocess.call(['wpa_cli', '-i', 'wlan0', 'remove_network', id])
 
-
+# Return id of the ssid from wpa_cli list
 def id_wpa_cli(ssid):
     proc = subprocess.Popen(f'wpa_cli -i wlan0 list_networks | grep {ssid}',
                             shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -68,7 +90,7 @@ def id_wpa_cli(ssid):
         return -1
     return output.split()[0]
 
-
+# Kill and reconnect wpa_supplicant
 def reconnect_wpa_supplicant(dir):
     MyOut1 = subprocess.Popen(['sudo', 'killall', 'wpa_supplicant'])
     MyOut1.wait()
@@ -82,20 +104,18 @@ def reconnect_wpa_supplicant(dir):
 
     print('reconnect end')
 
-
-def write_wpa_supplicant(ssid, psk):
-    proc = subprocess.Popen(f"wpa_passphrase '{ssid}' {psk} | sudo tee -a {wpa_supplicant}", 
+# Write ssid and password to wpa_supplicant.conf of the directory
+def write_wpa_supplicant(ssid, psw, dir):
+    proc = subprocess.Popen(f"wpa_passphrase '{ssid}' {psw} | sudo tee -a {dir}", 
                             shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout,stderr = proc.communicate()
     print('write wpa end')
 
-
-def write_temp_wpa_supplicant(ssid, psk):
-    proc = subprocess.Popen(f"wpa_passphrase '{ssid}' {psk} | sudo tee -a {user_directory}", 
-                            shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout,stderr = proc.communicate()
-    print('write temp wpa end')
-
-
+# Set the priority of the id in wpa_cli list
+def set_priority(id, priority):
+    subprocess.call(['wpa_cli', '-i', 'wlan0', 'set_network', id, 'priority', priority])
+    sleep(1)
+    subprocess.call(['wpa_cli', '-i', 'wlan0', 'save_config'])
+    sleep(1)
 
 
